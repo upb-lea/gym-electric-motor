2d860bbfc96e7914f13d132190cf44345aca2c0c
wkirgsn <wilkir@gmx.de>
2020-02-08 10:16:12 +0100

    add additional info to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='gym_electric_motor',
       version='0.0.3',
       description='An OpenAI gym environment for electric motor control.',
@@ -10,4 +13,8 @@ setup(name='gym_electric_motor',
       extra_requires={'examples': ['keras>=2.2.4',
                                    'keras_rl>=0.4.2',
                                    'rl>=3.0',
-                                   'tensorflow<=1.14']})
+                                   'tensorflow<=1.14']},
+      author='Arne Traue, Gerrit Book, Praneeth Balakrishna',
+      long_description=long_description,
+      url="https://github.com/upb-lea/gym-electric-motor",
+      )
