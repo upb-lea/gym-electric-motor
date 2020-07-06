21c48742b83b5f201ec177a8d4d1b2a166c9581a
Darius <dariusjakobeit@web.de>
2020-07-06 10:38:29 +0200

    Changed DummyVoltageSupply to fit new voltage returns

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -316,7 +316,7 @@ class DummyVoltageSupply(VoltageSupply):
         self.t = t
         self.args = args
         self.kwargs = kwargs
-        return self._u_nominal
+        return [self._u_nominal]
 
 
 class DummyConverter(PowerElectronicConverter):
