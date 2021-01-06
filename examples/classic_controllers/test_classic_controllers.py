f4e54180585e2aac62794435af8b9957667534cf
Felix Book <64723297+fbook98@users.noreply.github.com>
2021-01-06 10:43:22 +0100

    Update test_classic_controllers.py

--- examples/classic_controllers/test_classic_controllers.py
+++ examples/classic_controllers/test_classic_controllers.py
@@ -1,8 +1,7 @@
 from classic_controllers import Controller
-import sys, os
 
-sys.path.append(os.path.abspath(os.path.join('..')))
 import gym_electric_motor as gem
+from gym.spaces import Box
 from gym_electric_motor.reference_generators import StepReferenceGenerator, SinusoidalReferenceGenerator, SwitchedReferenceGenerator, MultipleReferenceGenerator, SubepisodedReferenceGenerator, WienerProcessReferenceGenerator, TriangularReferenceGenerator, ConstReferenceGenerator
 from gym_electric_motor.visualization import MotorDashboard
 from gym_electric_motor import reference_generators as rg
@@ -13,17 +12,18 @@ import numpy as np
 
 if __name__ == '__main__':
 
-ref = 'omega'
+
+    ref = 'omega'
     env = gem.make(
-        #'DcExtExDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1), motor_parameter=dict(j_rotor=0.00005), load_parameter={'a': 0, 'b': 0, 'c': 0, 'j_load': 0},
+        #'DcExtExDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
         #'DcPermExDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
         #'DcSeriesDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcShuntDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1), motor_parameter=dict(j_rotor=0.001), load_parameter={'a': 0, 'b': 0, 'c': 0, 'j_load': 0.001},
+        #'DcShuntDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
 
-        #'DcExtExCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1), motor_parameter=dict(j_rotor=0.00005), load_parameter={'a': 0, 'b': 0, 'c': 0, 'j_load': 0},
+        #'DcExtExCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
         'DcPermExCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
         #'DcSeriesCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcShuntCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1), motor_parameter=dict(j_rotor=0.001), load_parameter={'a': 0, 'b': 0, 'c': 0.5, 'j_load': 0},
+        #'DcShuntCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
 
         ode_solver='scipy.solve_ivp', solver_kwargs=dict(),
         #load=ConstantSpeedLoad(omega_fixed=50 * np.pi / 30),
@@ -36,35 +36,9 @@ ref = 'omega'
 
     )
 
+    controller = Controller.make(env)
 
-    try:
-        controller = Controller.make('pi_controller', env, p_gain= 5, param_dict={'p_gain': 10, 'i_gain': 15})
-        steps = 10000
-    except:
-        controller = Controller.make('three_point', env, param_dict={})
-        steps = 100000
-
-    controller = Controller.make('cascaded_controller', env,
-                                  inner_controller={'controller_type': 'pi_controller'},
-                                  outer_controller={'controller_type': 'pi_controller', 'p_gain': 10, 'i_gain': 15})
-    '''
-
-    controller = Controller.make('cascaded_controller', env,
-                                      inner_controller={'controller_type': 'three_point'},
-                                      outer_controller={'controller_type': 'on_off'})
-
-    
-    controller = Controller.make('cascaded_controller', env,
-                                  inner_controller={'controller_type': 'pi_controller'},
-                                  outer_controller={'controller_type': 'three_point'})
-
-    
-    
-    controller = Controller.make('cascaded_controller', env,
-                                  outer_controller={'controller_type': 'pid_controller'},
-                                  inner_controller={'controller_type': 'on_off'})
-    '''
-
+    steps = 10000 if type(env.action_space) == Box else 100000
 
     state, reference = env.reset()
     cum_rew = 0
@@ -73,9 +47,11 @@ ref = 'omega'
         env.render()
         action = controller.control(state, reference)
         (state, reference), reward, done, _ = env.step(action)
+
         if done:
             env.reset()
             controller.reset()
+
         cum_rew += reward
     print(cum_rew)
     env.close()
