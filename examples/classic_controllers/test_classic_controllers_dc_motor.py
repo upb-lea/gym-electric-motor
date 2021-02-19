173721579e9eaa703a559eb59b10fccf5f190607
Felix Book <64723297+fbook98@users.noreply.github.com>
2021-02-19 09:48:12 +0100

    Update and rename test_classic_controllers.py to test_classic_controllers_dc_motor.py

--- examples/classic_controllers/test_classic_controllers_dc_motor.py
+++ examples/classic_controllers/test_classic_controllers_dc_motor.py
@@ -3,10 +3,12 @@ from classic_controllers import Controller
 import gym_electric_motor as gem
 from gym_electric_motor.physical_systems.mechanical_loads import PolynomialStaticLoad, ConstantSpeedLoad
 from gym.spaces import Box
-from gym_electric_motor.reference_generators import StepReferenceGenerator, SinusoidalReferenceGenerator, SwitchedReferenceGenerator, MultipleReferenceGenerator, SubepisodedReferenceGenerator, WienerProcessReferenceGenerator, TriangularReferenceGenerator, ConstReferenceGenerator
 from gym_electric_motor.visualization import MotorDashboard
 from gym_electric_motor import reference_generators as rg
+from matplotlib import pyplot as plt
 import numpy as np
+import matplotlib
+
 
 
 if __name__ == '__main__':
@@ -34,20 +36,20 @@ if __name__ == '__main__':
                     ], p=[0.5, 0.25, 0.25], super_episode_length=(10000, 100000)
                 )
             )
-        reference_generator = MultipleReferenceGenerator(ref_gen)
+        reference_generator = rg.MultipleReferenceGenerator(ref_gen)
 
     limit_values = dict(omega=100*np.pi/30)
-
+    matplotlib.use('TkAgg')
     env = gem.make(
-        #'DcExtExDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
-        #'DcPermExDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcSeriesDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcShuntDisc-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
+        #'DcExtExDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
+        #'DcPermExDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i', 'u'], visu_period=1),
+        #'DcSeriesDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i', 'u'], visu_period=1),
+        #'DcShuntDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
 
-        #'DcExtExCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
-        'DcPermExCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcSeriesCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i', 'u'], visu_period=1),
-        #'DcShuntCont-v1', visualization=MotorDashboard(plots=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
+        #'DcExtExCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_a', 'i_e', 'u_a', 'u_e'], visu_period=1),
+        'DcPermExCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i', 'u'], visu_period=1),
+        #'DcSeriesCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i', 'u'], visu_period=1),
+        #'DcShuntCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_a', 'i_e', 'u'], visu_period=1),
 
         ode_solver='scipy.solve_ivp', solver_kwargs=dict(),
         #limit_values=limit_values,
@@ -57,18 +59,19 @@ if __name__ == '__main__':
     )
     controller = Controller.make(env)
 
-    steps = 10000 if type(env.action_space) == Box else 100000
+    steps = 10001 if type(env.action_space) == Box else 100001
     state, reference = env.reset()
     cum_rew = 0
 
     for i in range(steps):
-        env.render()
+
         action = controller.control(state, reference)
+        env.render(ref=controller.plot())
         (state, reference), reward, done, _ = env.step(action)
         if done:
             env.reset()
             controller.reset()
-
         cum_rew += reward
     print(cum_rew)
     env.close()
+    plt.show(block=True)
