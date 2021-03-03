497c18459079ddfbcc736c48c3e24990464adcf6
Felix Book <64723297+fbook98@users.noreply.github.com>
2021-03-03 12:41:20 +0100

    Update test_classic_controllers_synch_motor.py

--- examples/classic_controllers/test_classic_controllers_synch_motor.py
+++ examples/classic_controllers/test_classic_controllers_synch_motor.py
@@ -1,4 +1,5 @@
 from classic_controllers import Controller
+from externally_referenced_state_plot import ExternallyReferencedStatePlot
 import gym_electric_motor as gem
 from gym_electric_motor import reference_generators as rg
 from gym_electric_motor.visualization import MotorDashboard
@@ -9,11 +10,7 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    motor_parameter = dict(r_s=15e-3, l_d=0.37e-3, l_q=1.2e-3, p=3, j_rotor=0.06)
-    limit_values = dict(i=160 * 1.41, omega=12000 * np.pi / 30, u=450)
-    nominal_values = {key: 0.7 * limit for key, limit in limit_values.items()}
-
-    ref_states = ['i_sd', 'torque']
+    ref_states = ['i_sd', 'i_sq']
 
     q_generator = rg.SwitchedReferenceGenerator(
         sub_generators=[
@@ -24,7 +21,7 @@ if __name__ == '__main__':
         p=[0.3, 0.2, 0.3, 0.2], super_episode_length=(1000, 10000)
     )
 
-    '''
+
     d_generator = rg.SwitchedReferenceGenerator(
         sub_generators=[
             rg.TriangularReferenceGenerator(reference_state='i_sd', amplitude_range=(0, 0.3), offset_range=(0, 0.2)),
@@ -36,39 +33,34 @@ if __name__ == '__main__':
     )
     '''
     d_generator = rg.ConstReferenceGenerator(reference_state=ref_states[0], reference_value=0)
-
+    '''
     reference_generator = rg.MultipleReferenceGenerator([d_generator, q_generator])
+    external_ref_plots = [ExternallyReferencedStatePlot(state) for state in ['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq']]
     matplotlib.use('TkAgg')
     env = gem.make(
 
-        #'SynRMDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq'], visu_period=10),
-        #'PMSMDisc-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq'], visu_period=10),
+        #'AbcCont-TC-SynRM-v0', visualization=MotorDashboard(additional_plots=external_ref_plots),
+        'AbcCont-CC-PMSM-v0', visualization=MotorDashboard(additional_plots=external_ref_plots),
 
-        #'SynRMCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq'], visu_period=10),
-        'PMSMCont-v1', visualization=MotorDashboard(state_plots_extended=['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq'], visu_period=10),
+        ode_solver='scipy.solve_ivp',
 
-        ode_solver='scipy.solve_ivp', solver_kwargs=dict(),
-        load=ConstantSpeedLoad(omega_fixed=1000 * np.pi / 30),
         reference_generator=reference_generator,
-        motor_parameter=motor_parameter,
-        limit_values=limit_values,
-        nominal_values=nominal_values,
-        u_sup=400,
-
     )
 
-    controller = Controller.make(env)
+    controller = Controller.make(env, external_ref_plots=external_ref_plots)
     state, reference = env.reset()
 
     steps = 10001
     cum_rew = 0
 
     for i in range(steps):
+
         action = controller.control(state, reference)
-        env.render(ref=controller.plot())
+        env.render()
         (state, reference), reward, done, _ = env.step(action)
         if done:
-            state, reference = env.reset()
+            env.reset()
+            controller.reset()
         cum_rew += reward
     print(cum_rew)
     env.close()
