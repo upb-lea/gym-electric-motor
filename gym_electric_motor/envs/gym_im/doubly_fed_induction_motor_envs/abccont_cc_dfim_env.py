bad62a6828c1e37ff3bb2b6b3ae3d2288f6e5dd3
Arne <arnetraue@gmx.de>
2021-04-11 16:43:24 +0200

    DFIM Documentation Update

--- gym_electric_motor/envs/gym_im/doubly_fed_induction_motor_envs/abccont_cc_dfim_env.py
+++ gym_electric_motor/envs/gym_im/doubly_fed_induction_motor_envs/abccont_cc_dfim_env.py
@@ -11,92 +11,126 @@ from gym_electric_motor.constraints import SquaredConstraint
 
 class AbcContCurrentControlDoublyFedInductionMotorEnv(ElectricMotorEnvironment):
     """
-        Description:
-            Environment to simulate a abc-domain cont control set torque controlled doubly fed induction motor
+    Description:
+        Environment to simulate a abc-domain continuous control set current controlled doubly fed induction motor.
 
-        Key:
-            `AbcCont-CC-DFIM-v0`
+    Key:
+        ``'AbcCont-CC-DFIM-v0'``
 
-        Default Components:
-            Supply: IdealVoltageSupply
-            Converter: ContMultiConverter(ContB6BridgeConverter, ContB6BridgeConverter)
-            Motor: DoublyFedInductionMotor
-            Load: ConstantSpeedLoad
-            Ode-Solver: EulerSolver
-            Noise: None
+    Default Components:
+        - Supply: :py:class:`.IdealVoltageSupply`
+        - Converter: :py:class:`.ContB6BridgeConverter`
+        - Motor: :py:class:`.DoublyFedInductionMotor`
+        - Load: :py:class:`.ConstantSpeedLoad`
+        - Ode-Solver: :py:class:`.EulerSolver`
+        - Noise: **None**
 
-            Reference Generator:
-                WienerProcessReferenceGenerator
-                    Reference Quantity. 'i_sq' and 'i_sd'
+        - Reference Generator: :py:class:`.WienerProcessReferenceGenerator` *Reference Quantity:* ``'i_sd', 'i_sq``
 
-            Reward Function:
-                WeightedSumOfErrors: reward_weights 'i_sd' = 0.5, 'i_sq' = 0.5
+        - Reward Function: :py:class:`.WeightedSumOfErrors` reward_weights: ``'i_sd' = 0.5, 'i_sq' = 0.5``
 
-            Visualization:
-                MotorDashboard: current and action plots
+        - Visualization: :py:class:`.MotorDashboard` current and action plots
 
-            Constraints:
-                Squared Current Limit on 'i_sq' and 'i_sd'
+        - Constraints: :py:class:`.SquaredConstraint` on the currents  ``'i_sd', 'i_sq'``
 
-        Control Cycle Time:
-            tau = 1e-4 seconds
+    State Variables:
+        ``[
+            'omega' , 'torque',
+            'i_sa', 'i_sb', 'i_sc', 'i_sd', 'i_sq',
+            'u_sa', 'u_sb', 'u_sc', 'u_sd', 'u_sq',
+            'i_ra', 'i_rb', 'i_rc', 'i_rd', 'i_rq',
+            'u_ra', 'u_rb', 'u_rc', 'u_rd', 'u_rq',
+            'epsilon', 'u_sup'
+        ]``
 
-        State Variables:
-            ``['omega' , 'torque', 'i_a', 'i_b', 'i_c', 'i_sd', 'i_sq',
-             'u_a', 'u_b','u_c','u_sd','u_sq', 'u_sup', 'epsilon]``
+    Reference Variables:
+        ``['i_sd', 'i_sq']``
 
-        Observation Space:
-            Type: Tuple(State_Space, Reference_Space)
+    Control Cycle Time:
+        tau = 1e-4 seconds
 
-        State Space:
-            Box(low=[-1, -1, -1, -1, -1, -1, 0], high=[1, 1, 1, 1, 1, 1, 1])
+    Observation Space:
+        Type: Tuple(State_Space, Reference_Space)
 
-        Reference Space:
-            Box(low=[-1], high=[1])
+    State Space:
+        Box(low=13 * [-1], high=13 * [1])
 
-        Action Space:
-            Type: Box(low=[-1,-1,-1], high=[1,1,1])
+    Reference Space:
+        Box(low=[-1, -1], high=[1, 1])
 
-        Starting State:
-            Zeros on all state variables.
+    Action Space:
+        Box(low=[-1, -1, -1], high=[1, 1, 1])
 
-        Episode Termination:
-            Termination if current limits are violated.
-        """
-    def __init__(self, supply=None, converter=None, motor=None, load=None, ode_solver=None, noise_generator=None,
-                 reward_function=None, reference_generator=None, visualization=None, state_filter=None, callbacks=(),
-                 constraints=(SquaredConstraint(('i_sq', 'i_sd')),), calc_jacobian=True, tau=1e-4):
+    Initial State:
+        Zeros on all state variables.
+
+    Example:
+        >>> import gym_electric_motor as gem
+        >>> from gym_electric_motor.reference_generators import LaplaceProcessReferenceGenerator
+        >>>
+        >>> # Select a different ode_solver with default parameters by passing a keystring
+        >>> my_overridden_solver = 'scipy-solve_ivp'
+        >>>
+        >>> # Update the default arguments to the voltage supply by passing a parameter dict
+        >>> my_changed_voltage_supply_args = {'u_nominal': 400.0}
+        >>>
+        >>> # Replace the reference generator by passing a new instance
+        >>> my_new_ref_gen_instance = LaplaceProcessReferenceGenerator(
+        ...     reference_state='i_sq',
+        ...     sigma_range=(1e-3, 1e-2)
+        ... )
+        >>> env = gem.make(
+        ...     'AbcCont-CC-DFIM-v0',
+        ...     voltage_supply=my_changed_voltage_supply_args,
+        ...     ode_solver=my_overridden_solver,
+        ...     reference_generator=my_new_ref_gen_instance
+        ... )
+        >>> done = True
+        >>> for _ in range(1000):
+        >>>     if done:
+        >>>         state, reference = env.reset()
+        >>>     env.render()
+        >>>     (state, reference), reward, done, _ = env.step(env.action_space.sample())
+    """
+    def __init__(
+            self, supply=None, converter=None, motor=None, load=None, ode_solver=None, noise_generator=None,
+            reward_function=None, reference_generator=None, visualization=None, state_filter=None, callbacks=(),
+            constraints=(SquaredConstraint(('i_sq', 'i_sd')),), calc_jacobian=True, tau=1e-4
+    ):
         """
         Args:
-            supply(env-arg): Specification of the supply to be used in the environment
-            converter(env-arg): Specification of the converter to be used in the environment
-            motor(env-arg): Specification of the motor to be used in the environment
-            load(env-arg): Specification of the load to be used in the environment
-            ode_solver(env-arg): Specification of the ode_solver to be used in the environment
-            noise_generator(env-arg): Specification of the noise_generator to be used in the environment
-            reward_function(env-arg: Specification of the reward_function to be used in the environment
-            reference_generator(env-arg): Specification of the reference generator to be used in the environment
-            visualization(env-arg): Specification of the visualization to be used in the environment
-            constraints(iterable(str/Constraint)): All Constraints of the environment.
+            supply(env-arg): Specification of the :py:class:`.VoltageSupply` for the environment
+            converter(env-arg): Specification of the :py:class:`.PowerElectronicConverter` for the environment
+            motor(env-arg): Specification of the :py:class:`.ElectricMotor` for the environment
+            load(env-arg): Specification of the :py:class:`.MechanicalLoad` for the environment
+            ode_solver(env-arg): Specification of the :py:class:`.OdeSolver` for the environment
+            noise_generator(env-arg): Specification of the :py:class:`.NoiseGenerator` for the environment
+            reward_function(env-arg): Specification of the :py:class:`.RewardFunction` for the environment
+            reference_generator(env-arg): Specification of the :py:class:`.ReferenceGenerator` for the environment
+            visualization(env-arg): Specification of the :py:class:`.ElectricMotorVisualization` for the environment
+            constraints(iterable(str/Constraint)): All Constraints of the environment. \n
                 - str: A LimitConstraints for states (episode terminates, if the quantity exceeds the limit)
-                 can be directly specified by passing the state name here (e.g. 'i', 'omega')
-                 - instance of Constraint: More complex constraints (e.g. the SquaredConstraint can be initialized and
+                 can be directly specified by passing the state name here (e.g. 'i', 'omega') \n
+                - instance of Constraint: More complex constraints (e.g. the SquaredConstraint can be initialized and
                  passed to the environment.
             calc_jacobian(bool): Flag, if the jacobian of the environment shall be taken into account during the
                 simulation. This may lead to speed improvements. Default: True
-            tau(float): Duration of one control step in seconds. Default: 1e-5.
+            tau(float): Duration of one control step in seconds. Default: 1e-4.
             state_filter(list(str)): List of states that shall be returned to the agent. Default: None (no filter)
-            callbacks(list(Callback)): Callbacks for user interaction.
+            callbacks(list(Callback)): Callbacks for user interaction. Default: ()
 
         Note on the env-arg type:
             All parameters of type env-arg can be selected as one of the following types:
-                - instance: Pass an already instantiated object derived from the corresponding base class
-                    (e.g. reward_function=MyRewardFunction()). This is directly used in the environment.
-                - dict: Pass a dict to update the default parameters of the default type.
-                    (e.g. visualization=dict(state_plots=('omega', 'u')))
-                - str: Pass a string out of the registered classes to select a different class for the component.
-                    This class is then initialized with its default parameters.
-                    The available strings can be looked up in the documentation. (e.g. converter='Disc-2QC')
+
+            **instance:** Pass an already instantiated object derived from the corresponding base class
+            (e.g. ``reward_function=MyRewardFunction()``). This is directly used in the environment.
+
+            **dict:** Pass a dict to update the default parameters of the default type.
+            (e.g. ``visualization=dict(state_plots=('omega', 'u'))``)
+
+            **str:** Pass a string out of the registered classes to select a different class for the component.
+            This class is then initialized with its default parameters.
+            The available strings can be looked up in the documentation. (e.g. ``converter='Finite-2QC'``)
         """
         default_subgenerators = (
             WienerProcessReferenceGenerator(reference_state='i_sd'),
