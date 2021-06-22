7eee77ddd491508c6de0765bdf7b50c96a37e53d
Felix Book <felix.book98@gmail.com>
2021-06-22 08:22:05 +0200

    Add documentation

--- examples/classic_controllers/externally_referenced_state_plot.py
+++ examples/classic_controllers/externally_referenced_state_plot.py
@@ -2,6 +2,27 @@ from gym_electric_motor.visualization.motor_dashboard_plots import StatePlot
 
 
 class ExternallyReferencedStatePlot(StatePlot):
+    """Plot that displays environments states together with externally generated references.
+            These could be for example references that are generated intermediately within a cascaded controller.
+            Usage Example
+            -------------
+            .. code-block:: python
+                :emphasize-lines: 1,12
+                my_externally_referenced_plot = ExternallyReferencedStatePlot(state='i_sd')
+                env = gem.make(
+                    'DqCont-SC-PMSM-v0',
+                    visualization=dict(additional_plots=(my_externally_referenced_plot,),
+                )
+                done = True
+                for _ in range(10000):
+                    if done:
+                        state, reference = env.reset()
+                    external_reference_value = my_external_isd_reference_generator.get_reference()
+                    my_externally_referenced_plot.external_reference(external_reference_value)
+                    env.render()
+                    action = env.action_space.sample()
+                    (state, reference), reward, done, _ = env.step(action)
+    """
 
     def __init__(self, state):
         super().__init__(state)
