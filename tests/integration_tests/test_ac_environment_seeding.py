2d728d90388afe17f769fc8dd53dbb54d8957cfc
Arne <arnetraue@gmx.de>
2021-07-07 21:22:02 +0200

    Added missing comments on the tests

--- tests/integration_tests/test_ac_environment_seeding.py
+++ tests/integration_tests/test_ac_environment_seeding.py
@@ -16,16 +16,20 @@ seeds = [123, 456, 789]
 @pytest.mark.parametrize('action_type', action_types)
 @pytest.mark.parametrize('seed', seeds)
 def test_seeding_same_env(ac_motor, control_task, action_type, version, no_of_steps, seed):
+    """This test assures that a reseeding of the same environment leads to the same episodes again."""
     env_id = f'{action_type}-{control_task}-{ac_motor}-{version}'
     env = gem.make(env_id)
+    # Initial seed
     env.seed(seed)
+    # Sample some actions
     actions = [env.action_space.sample() for _ in range(no_of_steps)]
     done = True
+    # Data of the initial seed
     states1 = []
     references1 = []
     rewards1 = []
     done1 = []
-
+    # Simulate the environment with the sampled actions
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -35,6 +39,7 @@ def test_seeding_same_env(ac_motor, control_task, action_type, version, no_of_st
         references1.append(reference)
         done1.append(done)
 
+    # Reseed the environment
     env.seed(seed)
     done = True
     states2 = []
@@ -42,6 +47,7 @@ def test_seeding_same_env(ac_motor, control_task, action_type, version, no_of_st
     rewards2 = []
     done2 = []
 
+    # Execute the reseeded env with the same actions
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -50,6 +56,8 @@ def test_seeding_same_env(ac_motor, control_task, action_type, version, no_of_st
         states2.append(state)
         references2.append(reference)
         done2.append(done)
+
+    # Assure that the data of the initial and second seeding of the environment are equal
     references1 = np.array(references1).flatten()
     references2 = np.array(references2).flatten()
     assert(np.all(np.array(states1) == np.array(states2)))
@@ -65,17 +73,20 @@ def test_seeding_same_env(ac_motor, control_task, action_type, version, no_of_st
 @pytest.mark.parametrize('action_type', action_types)
 @pytest.mark.parametrize('seed', seeds)
 def test_seeding_new_env(ac_motor, control_task, action_type, version, no_of_steps, seed):
+    """This test assures that two equal environments that are seeded with the same seed generate the same episodes."""
     env_id = f'{action_type}-{control_task}-{ac_motor}-{version}'
     env = gem.make(env_id)
+    # Seed the first environment
     env.seed(seed)
+    # Sample actions
     actions = [env.action_space.sample() for _ in range(no_of_steps)]
     done = True
-
+    # Episode data of the first environment
     states1 = []
     references1 = []
     rewards1 = []
     done1 = []
-
+    # Simulate the environment
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -84,15 +95,16 @@ def test_seeding_new_env(ac_motor, control_task, action_type, version, no_of_ste
         states1.append(state)
         references1.append(reference)
         done1.append(done)
-
+    # Create a new environment with the same id
     env = gem.make(env_id)
+    # Seed the environment with the same seed
     env.seed(seed)
     done = True
     states2 = []
     references2 = []
     rewards2 = []
     done2 = []
-
+    # Execute the new environment
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -104,6 +116,7 @@ def test_seeding_new_env(ac_motor, control_task, action_type, version, no_of_ste
         references2.append(reference)
         done2.append(done)
 
+    # Assure that the episodes of both environments are equal
     assert (np.all(np.array(states1) == np.array(states2)))
     assert (np.all(np.array(references1).flatten() == np.array(references2).flatten()))
     assert (np.all(np.array(rewards1) == np.array(rewards2)))
--- tests/integration_tests/test_dc_environment_seeding.py
+++ tests/integration_tests/test_dc_environment_seeding.py
@@ -16,16 +16,19 @@ seeds = [123, 456, 789]
 @pytest.mark.parametrize('action_type', action_types)
 @pytest.mark.parametrize('seed', seeds)
 def test_seeding_same_env(dc_motor, control_task, action_type, version, no_of_steps, seed):
+    """This test assures that an environment that is seeded two times with the same seed generates the same episodes."""
     env_id = f'{action_type}-{control_task}-{dc_motor}-{version}'
     env = gem.make(env_id)
+    # Seed the environment initially
     env.seed(seed)
+    # Sample actions that are used in both executions
     actions = [env.action_space.sample() for _ in range(no_of_steps)]
     done = True
     states1 = []
     references1 = []
     rewards1 = []
     done1 = []
-
+    # Execute the env
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -35,13 +38,14 @@ def test_seeding_same_env(dc_motor, control_task, action_type, version, no_of_st
         references1.append(reference)
         done1.append(done)
 
+    # Seed the environment again with the same seed
     env.seed(seed)
     done = True
     states2 = []
     references2 = []
     rewards2 = []
     done2 = []
-
+    # Execute the environment again
     for i in range(no_of_steps):
         if done:
             state, reference = env.reset()
@@ -50,6 +54,8 @@ def test_seeding_same_env(dc_motor, control_task, action_type, version, no_of_st
         states2.append(state)
         references2.append(reference)
         done2.append(done)
+
+    # Assure that the epsiodes of the initially and reseeded environment are equal
     references1 = np.array(references1).flatten()
     references2 = np.array(references2).flatten()
     assert(np.all(np.array(states1) == np.array(states2)))
@@ -65,6 +71,7 @@ def test_seeding_same_env(dc_motor, control_task, action_type, version, no_of_st
 @pytest.mark.parametrize('action_type', action_types)
 @pytest.mark.parametrize('seed', seeds)
 def test_seeding_new_env(dc_motor, control_task, action_type, version, no_of_steps, seed):
+    """This test assures that two equal environments that are seeded with the same seed generate the same episodes."""
     env_id = f'{action_type}-{control_task}-{dc_motor}-{version}'
     env = gem.make(env_id)
     env.seed(seed)
@@ -104,6 +111,7 @@ def test_seeding_new_env(dc_motor, control_task, action_type, version, no_of_ste
         references2.append(reference)
         done2.append(done)
 
+    # Assure that the episodes of both environments are equal
     assert (np.all(np.array(states1) == np.array(states2)))
     assert (np.all(np.array(references1).flatten() == np.array(references2).flatten()))
     assert (np.all(np.array(rewards1) == np.array(rewards2)))
