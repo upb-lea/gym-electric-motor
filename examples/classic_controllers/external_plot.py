ffae0c3e3fe5a6af7cc36e67cc45e48d5a9fa98c
Felix Book <felix.book98@gmail.com>
2021-10-05 09:36:08 +0200

    Update external_plot.py

--- examples/classic_controllers/external_plot.py
+++ examples/classic_controllers/external_plot.py
@@ -8,25 +8,46 @@ class ExternalPlot(TimePlot):
         lines can be plotted.
         Usage Example
         -------------
-            if __name__ == '__main__':
-                external_plot = ExternalPlot(min=-1, max=1, reference=True, additional_lines=2)
+            >>> from classic_controllers import Controller
+            >>> from external_plot import ExternalPlot
+            >>> import gym_electric_motor as gem
+            >>> from gym_electric_motor.visualization import MotorDashboard
+            >>> import numpy as np
 
-                env = gem.make('DqCont-CC-PMSM-v0', visualization=MotorDashboard(state_plots=['i_sd', 'i_sq'],
-                                                                                additional_plots=(external_plot,)))
+            >>> if __name__ == '__main__':
+            >>>     #define ExternalPlot Object with reference and two additional lines
+            >>>     external_plot = ExternalPlot(min=-1, max=1, referenced=True, additional_lines=2)
 
-                external_plot.set_label({'y_label': 'y', 'state_label': '$state$', 'ref_label': '$reference$',
-                                        'add_label': ['$add_1$', '$add_2$']})
-                done = True
-                for t in range(100000):
-                if done:
-                    state, reference = env.reset()
-                env.render()
-                data = [np.sin(t / 500), np.sin(t / 1000), np.sin(t / 1500), np.sin(t / 2000)]
-                external_plot.add_data(data)
-                (state, reference), reward, done, _ = env.step([0, 0])
+            >>>     # define gem environment and pass the ExternalPlot object as an additional plot
+            >>>     env = gem.make('DqCont-CC-PMSM-v0', visualization=MotorDashboard(state_plots=['i_sd', 'i_sq'],
+            ...                                                                     additional_plots=(external_plot,)))
+
+            >>>     # setting the labels of the plots
+            >>>     external_plot.set_label({'y_label': 'y', 'state_label': '$state$', 'ref_label': '$reference$',
+            ...                             'add_label': ['$add_1$', '$add_2$']})
+            >>>     done = True
+            >>>     for t in range(100000):
+            >>>     if done:
+            >>>         state, reference = env.reset()
+            >>>     env.render()
+            >>>     data = [np.sin(t / 500), np.sin(t / 1000), np.sin(t / 1500), np.sin(t / 2000)]
+            >>>     external_plot.add_data(data) # passing the data to the external plot
+            >>>     (state, reference), reward, done, _ = env.step([0, 0])
     """
 
     def __init__(self, referenced=False, additional_lines=0, min=0, max=1):
+        """
+            This function creates an object for external plots in a GEM Motordashboard.
+
+            Args:
+                referenced: a reference is to be displayed
+                additional_lines: number of additional lines in plot
+                min: minimum y-value of the plot
+                max: maximum y-value of the plot
+
+            Returns:
+                Object that can be passed to a GEM environment to plot additional data.
+        """
         super().__init__()
 
         self._state_line_config = self._default_time_line_cfg.copy()
@@ -87,12 +108,14 @@ class ExternalPlot(TimePlot):
 
         self._y_data = [self._state_data, self._reference_data]
 
+        # Check if there are added lines and define these lines
         if self.added:
             for i in range(self.add_lines):
                 self._additional_lines[i], = self._axis.plot(self._x_data, self._additional_data[i], **self._add_line_config, zorder=self.add_lines-i)
                 self._lines.append(self._additional_lines[i])
                 self._y_data.append(self._additional_data[i])
 
+        # Check if there is a reference and define the line
         if self._referenced:
             if self.added:
                 lines = [self._state_line, self._reference_line]
@@ -106,7 +129,11 @@ class ExternalPlot(TimePlot):
             self._axis.legend((self._state_line, ), (self.state_label, ), loc='upper left', numpoints=20)
 
     def set_label(self, labels):
-        """Method to set the labels"""
+        """
+            Method to set the labels, A dict must be passed. The keys are: y_label, state_label, ref_label, add_label.
+            For the key add_label a list with the length of the number of additional lines is passed.
+        """
+
         self._label = labels.get('y_label', '')
         self.state_label = labels['state_label']
         if self._referenced:
@@ -120,7 +147,7 @@ class ExternalPlot(TimePlot):
         self._x_data[idx] = self._t
 
     def add_data(self, additional_data):
-        """Method to pass the external data"""
+        """Method to pass the external data. A list must be passed with the length of the number of plots."""
         idx = self.data_idx
         if self._referenced:
             self._state_data[idx] = additional_data[0]
