b90ef4c7fc42e440eefd8f80506f64a676153a62
Arne <arnetraue@gmx.de>
2022-01-16 19:49:50 +0100

    Added the possibility to remove the angle to the CosSinProcessor

--- gym_electric_motor/state_action_processors/cos_sin_processor.py
+++ gym_electric_motor/state_action_processors/cos_sin_processor.py
@@ -7,6 +7,8 @@ from gym_electric_motor.state_action_processors import StateActionProcessor
 class CosSinProcessor(StateActionProcessor):
     """Adds ``cos(angle)`` and ``sin(angle)`` states to the systems state vector that are the cosine and sine of a
     certain systems state.
+
+    Optionally, the CosSinProcessor can also remove the angle from the state vector.
     """
 
     @property
@@ -14,27 +16,34 @@ class CosSinProcessor(StateActionProcessor):
         """Returns the name of the state whose cosine and sine are appended to the state vector."""
         return self._angle
 
-    def __init__(self, angle='epsilon', physical_system=None):
+    def __init__(self, angle='epsilon', physical_system=None, remove_angle=False):
         """
         Args:
             angle(string): Name of the state whose cosine and sine will be added to the systems state vector.
                 Default: ``'epsilon'``
             physical_system(PhysicalSystem(optional)): Inner system of this processor.
+            remove_angle(bool): Remove the angle from the state vector
         """
         self._angle = angle
         self._angle_index = None
+        self._remove_angle = remove_angle
+        self._remove_idx = []
         super().__init__(physical_system)
 
     def set_physical_system(self, physical_system):
         # Docstring of super class
         super().set_physical_system(physical_system)
-        low = np.concatenate((physical_system.state_space.low, [-1., -1.]))
-        high = np.concatenate((physical_system.state_space.high, [1., 1.]))
-        self.state_space = gym.spaces.Box(low, high, dtype=np.float64)
         self._angle_index = physical_system.state_positions[self._angle]
-        self._limits = np.concatenate((physical_system.limits, [1., 1.]))
-        self._nominal_state = np.concatenate((physical_system.nominal_state, [1., 1.]))
-        self._state_names = physical_system.state_names + [f'cos({self._angle})', f'sin({self._angle})']
+
+        self._remove_idx = [self._angle_index] if self._remove_angle else []
+        low = np.concatenate((np.delete(physical_system.state_space.low, self._remove_idx), [-1., -1.]))
+        high = np.concatenate((np.delete(physical_system.state_space.high, self._remove_idx), [1., 1.]))
+        self.state_space = gym.spaces.Box(low, high, dtype=np.float64)
+
+        self._limits = np.concatenate((np.delete(physical_system.limits, self._remove_idx), [1., 1.]))
+        self._nominal_state = np.concatenate((np.delete(physical_system.nominal_state, self._remove_idx), [1., 1.]))
+        self._state_names = list(np.delete(physical_system.state_names, self._remove_idx)) \
+            + [f'cos({self._angle})', f'sin({self._angle})']
         self._state_positions = {key: index for index, key in enumerate(self._state_names)}
         return self
 
@@ -46,7 +55,20 @@ class CosSinProcessor(StateActionProcessor):
     def simulate(self, action):
         # Docstring of super class
         state = self._physical_system.simulate(action)
+        if self._remove_angle:
+            return self._delete_angle(self._get_cos_sin(state))
         return self._get_cos_sin(state)
+
+    def _delete_angle(self, state):
+        """Removes the angle from the state vector
+
+        Args:
+            state(numpy.ndarray[float]): The state vector of the system.
+
+        Returns:
+            numpy.ndarray[float]: The state vector removed by the angle.
+                """
+        return np.delete(state, self._remove_idx)
 
     def _get_cos_sin(self, state):
         """Appends the cosine and sine of the specified state to the state vector.
