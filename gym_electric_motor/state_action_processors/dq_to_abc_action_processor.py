3c2f9d59ac5897f4dcc7ffd1cac2e5aaef255fa4
Arne <arnetraue@gmx.de>
2022-01-24 20:23:28 +0100

    DFIM DqToAbcProcessor compatibility

--- gym_electric_motor/state_action_processors/dq_to_abc_action_processor.py
+++ gym_electric_motor/state_action_processors/dq_to_abc_action_processor.py
@@ -38,45 +38,50 @@ class DqToAbcActionProcessor(StateActionProcessor):
 
         super().__init__(physical_system)
 
-
     def set_physical_system(self, physical_system):
         # Docstring of super class
         assert isinstance(physical_system.electrical_motor, ps.ThreePhaseMotor), \
             'The motor in the system has to derive from the ThreePhaseMotor to define transformations.'
         super().set_physical_system(physical_system)
-
-        if isinstance(physical_system, ps.DoublyFedInductionMotorSystem):
-            self._set_dfim_system(physical_system)
-            return self
-        self._action_space = gym.spaces.Box(-1, 1, shape=(2,), dtype=np.float64)
-        # If no angle name was passed, try to use defaults. ('epsilon' for sync motors 'psi_angle' for induction motors)
-        if self._angle_name is None:
-            if isinstance(physical_system.electrical_motor,
-                          (ps.PermanentMagnetSynchronousMotor, ps.SynchronousReluctanceMotor)):
-                self._angle_name = 'epsilon'
-            elif isinstance(physical_system.electrical_motor, ps.SquirrelCageInductionMotor):
-                self._angle_name = 'psi_angle'
-
-        assert self._angle_name in physical_system.state_names, \
-            f'Angle {self._angle_name} not in the states of the physical system. Probably a flux observer is required.'
-
-        self._angle_index = physical_system.state_names.index(self._angle_name)
         self._omega_index = physical_system.state_names.index('omega')
+        if isinstance(physical_system.unwrapped, ps.DoublyFedInductionMotorSystem):
+            self._set_dfim_system(physical_system)
+        else:
+            self._action_space = gym.spaces.Box(-1, 1, shape=(2,), dtype=np.float64)
+            # If no angle name was passed, try to use defaults.
+            # ('epsilon' for sync motors 'psi_angle' for induction motors)
+            if self._angle_name is None:
+                if isinstance(
+                    physical_system.electrical_motor,
+                    (ps.PermanentMagnetSynchronousMotor, ps.SynchronousReluctanceMotor)
+                ):
+                    self._angle_name = 'epsilon'
+                elif isinstance(physical_system.electrical_motor, ps.SquirrelCageInductionMotor):
+                    self._angle_name = 'psi_angle'
+            self.simulate = self._single_angle_simulate
+            self._angle_index = physical_system.state_names.index(self._angle_name)
+            assert self._angle_name in physical_system.state_names, \
+                f'Angle {self._angle_name} not in the states of the physical system. ' \
+                f'Probably a flux observer is required.'
+
         self._angle_advance = 0.5
 
         # If dead time has been added to the system increase the angle advance by the amount of dead time steps
         if hasattr(physical_system, 'dead_time'):
             self._angle_advance += physical_system.dead_time
-        self.simulate = self._single_angle_simulate
+
         return self
 
     def _set_dfim_system(self, physical_system):
         self._action_space = gym.spaces.Box(-1, 1, shape=(4,), dtype=np.float64)
-        raise NotImplementedError
+        self._angle_index = [
+            physical_system.state_names.index('psi_angle'), physical_system.state_names.index('epsilon')
+        ]
+        self.simulate = self._dfim_simulate
 
     def simulate(self, action):
         # Docstring of super class
-        raise ReferenceError('Physical System is still unset.')
+        raise ReferenceError('Physical System is unset.')
 
     def _single_angle_simulate(self, action):
         advanced_angle = self._state[self._angle_index] \
@@ -88,13 +93,13 @@ class DqToAbcActionProcessor(StateActionProcessor):
 
     def _dfim_simulate(self, action):
         # Docstring of super class
-        advanced_angle = self._state[self._angle_index] \
+        advanced_angle = self._state[self._angle_index[1]] \
             + self._angle_advance * self._physical_system.tau * self._state[self._omega_index]
-        dq_action_stator = action[:3]
-        dq_action_rotor = action[3:]
+        dq_action_stator = action[:2]
+        dq_action_rotor = action[2:]
         abc_action_stator = self._transformation(dq_action_stator, advanced_angle)
         abc_action_rotor = self._transformation(dq_action_rotor, self._angle_index[0] - advanced_angle)
-        abc_action = np.concatenate((abc_action_stator, abc_action_rotor)).tolist()
+        abc_action = np.concatenate((abc_action_stator, abc_action_rotor))
         normalized_state = self._physical_system.simulate(abc_action)
         self._state = normalized_state * self._physical_system.limits
         return normalized_state
