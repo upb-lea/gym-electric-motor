134da43a5ff0e86f1bf6ab0e5fbfe766bfdbee89
Arne <arnetraue@gmx.de>
2022-02-05 21:51:53 +0100

    design change

--- gym_electric_motor/state_action_processors/dq_to_abc_action_processor.py
+++ gym_electric_motor/state_action_processors/dq_to_abc_action_processor.py
@@ -23,7 +23,20 @@ class DqToAbcActionProcessor(StateActionProcessor):
         """
         return ps.ThreePhaseMotor.t_32(ps.ThreePhaseMotor.q_inv(action, angle))
 
-    def __init__(self, angle_name=None, physical_system=None):
+    _registry = {}
+
+    @classmethod
+    def register_transformation(cls, motor_types):
+            def wrapper(callable_):
+                for motor_type in motor_types:
+                    cls._registry[motor_type] = callable_
+            return wrapper
+
+    def __new__(cls, motor_type, *args, **kwargs):
+        assert motor_type in cls._registry.keys(), f'Not supported motor_type {motor_type}.'
+        return cls._registry[motor_type](*args, **kwargs)
+
+    def __init__(self, angle_name, physical_system=None):
         """
         Args:
             angle_name(string): Name of the state that defines the current angle of the AC-motor
@@ -44,25 +57,10 @@ class DqToAbcActionProcessor(StateActionProcessor):
             'The motor in the system has to derive from the ThreePhaseMotor to define transformations.'
         super().set_physical_system(physical_system)
         self._omega_index = physical_system.state_names.index('omega')
-        if isinstance(physical_system.unwrapped, ps.DoublyFedInductionMotorSystem):
-            self._set_dfim_system(physical_system)
-        else:
-            self._action_space = gym.spaces.Box(-1, 1, shape=(2,), dtype=np.float64)
-            # If no angle name was passed, try to use defaults.
-            # ('epsilon' for sync motors 'psi_angle' for induction motors)
-            if self._angle_name is None:
-                if isinstance(
-                    physical_system.electrical_motor,
-                    (ps.PermanentMagnetSynchronousMotor, ps.SynchronousReluctanceMotor)
-                ):
-                    self._angle_name = 'epsilon'
-                elif isinstance(physical_system.electrical_motor, ps.SquirrelCageInductionMotor):
-                    self._angle_name = 'psi_angle'
-            self.simulate = self._single_angle_simulate
-            self._angle_index = physical_system.state_names.index(self._angle_name)
-            assert self._angle_name in physical_system.state_names, \
-                f'Angle {self._angle_name} not in the states of the physical system. ' \
-                f'Probably a flux observer is required.'
+        self._angle_index = physical_system.state_names.index(self._angle_name)
+        assert self._angle_name in physical_system.state_names, \
+            f'Angle {self._angle_name} not in the states of the physical system. ' \
+            f'Probably a flux observer is required.'
 
         self._angle_advance = 0.5
 
@@ -72,33 +70,51 @@ class DqToAbcActionProcessor(StateActionProcessor):
 
         return self
 
-    def _set_dfim_system(self, physical_system):
-        self._action_space = gym.spaces.Box(-1, 1, shape=(4,), dtype=np.float64)
-        self._angle_index = [
-            physical_system.state_names.index('psi_angle'), physical_system.state_names.index('epsilon')
-        ]
-        self.simulate = self._dfim_simulate
+    def simulate(self, action):
+        raise NotImplementedError
+
+    def reset(self, **kwargs):
+        # Docstring of super class
+        normalized_state = self._physical_system.reset()
+        self._state = normalized_state * self._physical_system.limits
+        return normalized_state
+
+    def _advance_angle(self, state):
+        return state[self._angle_index] \
+            + self._angle_advance * self._physical_system.tau * state[self._omega_index]
+
+
+class _ClassicDqToAbcActionProcessor(DqToAbcActionProcessor):
+
+    @property
+    def action_space(self):
+        return gym.spaces.Box(-1, 1, shape=(2,), dtype=np.float64)
 
     def simulate(self, action):
-        # Docstring of super class
-        raise ReferenceError('Physical System is unset.')
-
-    def _single_angle_simulate(self, action):
-        """Dq to abc space transformation function for all environments except those for a doubly fed induction motor.
-
-        Args:
-            action: The actions for the stator and rotor circuit in dq-coordinates.
-        Returns:
-            The next state of the physical system.
-        """
-        advanced_angle = self._state[self._angle_index] \
-            + self._angle_advance * self._physical_system.tau * self._state[self._omega_index]
+        # Docstring of superclass
+        advanced_angle = self._advance_angle(self._state)
         abc_action = self._transformation(action, advanced_angle)
         normalized_state = self._physical_system.simulate(abc_action)
         self._state = normalized_state * self._physical_system.limits
         return normalized_state
 
-    def _dfim_simulate(self, action):
+
+DqToAbcActionProcessor.register_transformation(['PMSM'])(
+    lambda angle_name='epsilon', *args, **kwargs: _ClassicDqToAbcActionProcessor(angle_name, *args, **kwargs)
+)
+
+DqToAbcActionProcessor.register_transformation(['SCIM'])(
+    lambda angle_name='psi_angle', *args, **kwargs: _ClassicDqToAbcActionProcessor(angle_name, *args, **kwargs)
+)
+
+
+class _DFIMDqToAbcActionProcessor(DqToAbcActionProcessor):
+
+    @property
+    def action_space(self):
+        return gym.spaces.Box(-1, 1, shape=(4,))
+
+    def simulate(self, action):
         """Dq to abc space transformation function for doubly fed induction motor environments.
 
         Args:
@@ -114,11 +130,5 @@ class DqToAbcActionProcessor(StateActionProcessor):
         abc_action_rotor = self._transformation(dq_action_rotor, self._angle_index[0] - advanced_angle)
         abc_action = np.concatenate((abc_action_stator, abc_action_rotor))
         normalized_state = self._physical_system.simulate(abc_action)
-        self._state = normalized_state * self._physical_system.limits
-        return normalized_state
-
-    def reset(self, **kwargs):
-        # Docstring of super class
-        normalized_state = self._physical_system.reset()
         self._state = normalized_state * self._physical_system.limits
         return normalized_state
