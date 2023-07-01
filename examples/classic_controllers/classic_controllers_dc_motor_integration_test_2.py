9710ca88c45060e32d62fea8182845a1d40cf420
KoehlerM173 <koehler.marius@outlook.de>
2023-07-01 16:25:36 +0200

    new integration test draft

--- examples/classic_controllers/classic_controllers_dc_motor_integration_test_2.py
+++ examples/classic_controllers/classic_controllers_dc_motor_integration_test_2.py
@@ -2,6 +2,8 @@ from classic_controllers import Controller
 from externally_referenced_state_plot import ExternallyReferencedStatePlot
 import gym_electric_motor as gem
 from gym_electric_motor.visualization import MotorDashboard
+from gym_electric_motor.reference_generators import SinusoidalReferenceGenerator
+import time
 
 if __name__ == '__main__':
 
@@ -20,7 +22,7 @@ if __name__ == '__main__':
     """
 
     motor_type = 'PermExDc'
-    control_type = 'TC'
+    control_type = 'SC'
     action_type = 'Cont'
 
     motor = action_type + '-' + control_type + '-' + motor_type + '-v0'
@@ -37,8 +39,23 @@ if __name__ == '__main__':
     # definition of the plotted variables
     external_ref_plots = [ExternallyReferencedStatePlot(state) for state in states]
 
+    # definition of the reference generator
+
+    ref_generator = SinusoidalReferenceGenerator(amplitude_range= (100,100),
+                                                 frequency_range= (5,5),
+                                                 offset_range = (0,0),
+                                                 episode_lengths = (10001, 10001))
+
     # initialize the gym-electric-motor environment
-    env = gem.make(motor, visualization=MotorDashboard(additional_plots=external_ref_plots), render_mode="file")
+    env = gem.make(motor, 
+                   visualization=MotorDashboard(additional_plots=external_ref_plots),
+                   render_mode="figure", 
+                   reference_generator = ref_generator)
+    
+    env.metadata["filename_prefix"] = "integration-test"
+    env.metadata["filename_suffix"] = ""
+    env.metadata["save_figure_on_close"] = False
+    env.metadata["hold_figure_on_close"] = True
     """
         initialize the controller
 
@@ -50,16 +67,20 @@ if __name__ == '__main__':
             a (optional)                    tuning parameter of the symmetrical optimum (default: 4)
     
     """
-    visualization = MotorDashboard(additional_plots=external_ref_plots)
     controller = Controller.make(env, external_ref_plots=external_ref_plots)
 
-    state, reference = env.reset(seed = None)
+    state, reference = env.reset(seed=1337)
     # simulate the environment
     for i in range(10001):
         action = controller.control(state, reference)
+        #if i % 100 == 0:
+         #   (state, reference), reward, terminated, truncated, _ = env.step(env.action_space.sample())
+        #else:
         (state, reference), reward, terminated, truncated, _ = env.step(action)
+
+
         if terminated:
             env.reset()
             controller.reset()
-
+    
     env.close()
