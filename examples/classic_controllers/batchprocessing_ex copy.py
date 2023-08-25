efef08b7e6cf1cfc5499f10085e982540c2f8990
KoehlerM173 <77018066+KoehlerM173@users.noreply.github.com>
2023-08-25 14:00:03 +0200

    removed unnecessary code

--- examples/classic_controllers/batchprocessing_ex copy.py
+++ examples/classic_controllers/batchprocessing_ex copy.py
@@ -5,8 +5,6 @@ from externally_referenced_state_plot import ExternallyReferencedStatePlot
 import gym_electric_motor as gem
 from gym_electric_motor.visualization import MotorDashboard
 from gymnasium.vector import make as make_vec_env
-import multiprocessing
-import pydevd 
 
 def make_gem_env(motor, num_envs ,visualization, render_mode):
     return gem.make(motor, num_envs ,visualization, render_mode)
@@ -32,13 +30,14 @@ if __name__ == '__main__':
     external_ref_plots = [ExternallyReferencedStatePlot(state) for state in states]
 
         # initialize the gym-electric-motor environment
-    env = make_vec_env(motor, num_envs = 2 , env_fn = make_gem_env(motor, 2,visualization=MotorDashboard(additional_plots=external_ref_plots), render_mode="figure_once"))
+    env = make_vec_env(motor, num_envs = 2 , env_fn = make_gem_env(motor, 2,visualization=MotorDashboard(additional_plots=external_ref_plots), 
+                                                                   render_mode="figure"))
 
     state, reference = env.reset(seed=42)
 
-    actions = np.array([1, 0])
 
     env_buff = gem.make(motor, visualization=MotorDashboard(additional_plots=external_ref_plots), render_mode="figure")
+
     controller1 = Controller.make(env_buff, external_ref_plots=external_ref_plots)
     controller2 = Controller.make(env_buff, external_ref_plots=external_ref_plots)
 
