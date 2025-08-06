1fccec9aa0f7b78f8432ae247d0d4574ec2e6f2c
HauckeBa <barnabas.haucke-korber@ieee.org>
2025-08-06 15:37:30 +0200

    try another path to enable the full doc build

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 sys.setrecursionlimit(1500)
 # -- Project information -----------------------------------------------------
 
