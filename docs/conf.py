d37cc07a2fec29481e6444f460365596893327d3
mounica3003 <mounicav3003@gmail.com>
2025-08-15 14:58:57 +0200

    changing theme in confy

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx_rtd_theme',
     'sphinx_mdinclude',
 ]
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
