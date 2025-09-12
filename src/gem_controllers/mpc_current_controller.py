31f5c8810a98bb539deae4c7d81f6b9f75941a8b
manjeetjha070 <manjeetjha070@gmail.com>
2025-09-12 15:18:45 +0200

    I have updated my MPC controller that takes motor dynamics from model_constants

--- src/gem_controllers/mpc_current_controller.py
+++ src/gem_controllers/mpc_current_controller.py
@@ -59,22 +59,22 @@ class MPCCurrentController(GemController):
         # State indices
         self.i_sd_idx = self.state_names.index('i_sd')
         self.i_sq_idx = self.state_names.index('i_sq') 
-        self.omega_idx = self.state_names.index('omega')
-        self.epsilon_idx = self.state_names.index('epsilon')
+        self.omega_idx = self.state_names.index('omega')        
         self.u_sd_idx = self.state_names.index('u_sd')
         self.u_lim = self.limits[self.u_sd_idx]
 
         # Coordinate transformation
         self.abc_to_dq = self.physical_system.abc_to_dq_space
-        
-        # Finite action set
+       
         self.subactions = -np.power(-1, self.physical_system._converter._subactions)
+        
         # All possible voltage vectors in abc coordinates  
         self.u_abc_k1 = self.u_lim * self.subactions
         
         self.prediction_horizon = prediction_horizon
         
         print("MPCCurrentController initialized")
+
         # Model constants for MPC
         self._model_constants = self.physical_system.electrical_motor._model_constants  
         motor_type = type(self.physical_system.electrical_motor).__name__
@@ -82,49 +82,57 @@ class MPCCurrentController(GemController):
         if motor_type == "PermanentMagnetSynchronousMotor":
             self.motor_state_names = ["i_sd", "i_sq", "epsilon"]
         elif motor_type == "SynchronousReluctanceMotor":
-            self.motor_state_names = ["i_sd", "i_sq", "epsilon"]
-        elif motor_type == "ExternallyExcitedSynchronousMotor":
-            self.motor_state_names = ["i_sd", "i_sq", "i_e", "epsilon"]
-        elif motor_type == "InductionMotor":
-            self.motor_state_names = ["i_salpha", "i_sbeta", "psi_ralpha", "psi_rbeta", "epsilon"]
+            self.motor_state_names = ["i_sd", "i_sq", "epsilon"]        
         else:
          raise NotImplementedError(f"MPC controller not implemented for motor type: {motor_type}")        
           
         
 
-    def _simulate_sequence(self, x, A, g, omega, ref_i_d, ref_i_q, depth):
+    def _simulate_sequence(self,model_constants,x,omega,omega_Isd,omega_Isq, ref_i_d, ref_i_q, depth):
         min_cost = float('inf')
         best_sequence = []
 
         for idx, (v_a, v_b, v_c) in enumerate(self.u_abc_k1):
-            # Predict next state
+            # Transform voltages to dq frame
             v_dq = np.transpose(
                 np.array([self.abc_to_dq(np.array([v_a, v_b, v_c]), x[-1] + 0.5 * omega * self.tau)])
             )
             v_d, v_q = v_dq[0], v_dq[1]
 
-            # Voltage input contribution (from motor parameters)
-            u_contrib = np.array([
-                float(v_d) / self.l_d,
-                float(v_q) / self.l_q,
-                0.0
+            # Build input vector: [omega, i_d, i_q, u_d, u_q, omega*i_d, omega*i_q]
+            exterded_vector = np.array([
+                omega,
+                x[0],       # i_d
+                x[1],       # i_q
+                float(v_d),      # u_d
+                float(v_q),      # u_q
+                omega_Isd,
+                omega_Isq
             ])
 
             # Compute derivative
-            dx = A @ x + g + u_contrib
-
+            dx = model_constants @ exterded_vector  
+            
             # Euler integration for next step
             x_next = x + self.tau * dx
 
-            cost = (x_next[0] - ref_i_d) ** 2 + (x_next[1] - ref_i_q) ** 2
+            # Extract next states
+            i_d_next, i_q_next, epsilon_next = x_next
+            omega_Isd_next = omega * i_d_next
+            omega_Isq_next = omega * i_q_next
+            
+            # Calculate cost
+            cost = (i_d_next  - ref_i_d) ** 2 + (i_q_next - ref_i_q) ** 2
 
             if depth == self.prediction_horizon - 1:
                 total_cost = cost
                 sequence = [idx]
             else:
                 future_cost, future_sequence = self._simulate_sequence(
-                    x_next, A, g, omega, ref_i_d, ref_i_q, depth + 1
-                )
+                model_constants, x_next, omega,
+                omega_Isd_next, omega_Isq_next,
+                ref_i_d, ref_i_q, depth + 1
+            )
                 total_cost = cost + future_cost
                 sequence = [idx] + future_sequence
 
@@ -135,37 +143,28 @@ class MPCCurrentController(GemController):
         return min_cost, best_sequence
 
     def control(self, state, reference):
-        """Main control method matching PI controller interface"""
-        
-        # Denormalize state and references        
-        omega = self.p * state[self.omega_idx] * self.limits[self.omega_idx]
-
-        ref_i_q = reference[0] * self.limits[self.i_sq_idx]
-        ref_i_d = reference[1] * self.limits[self.i_sq_idx]
-
+        """Main control method matching PI controller interface"""      
+    
         # Build denormalized state vector for the motor (only relevant states)
         state_vector = []
         for name in self.motor_state_names:
             s_idx = self.state_names.index(name)
             state_vector.append(state[s_idx] * self.limits[s_idx])
-        x = np.array(state_vector)
+        x = np.array(state_vector)  
 
-        # Unpack electrical jacobian
-        A, g, dTdx = self.physical_system.electrical_motor.electrical_jacobian(
-            x,  # full state vector
-            u_in=0,  # inputs (or adapt for your motor)
-            omega=omega
-        )
-        # Local indices for motor states
-        self.local_idx = {name: i for i, name in enumerate(self.motor_state_names)}
+        # Denormalize state and references        
+        omega = state[self.omega_idx] * self.limits[self.omega_idx]           
+        ref_i_q = reference[0] * self.limits[self.i_sq_idx]
+        ref_i_d = reference[1] * self.limits[self.i_sq_idx]  
+        omega_Isd = omega * x[0]  # omega * i_sd
+        omega_Isq = omega * x[1]  # omega * i_sq    
 
-        # remove duplicated cross-coupling terms from g
-        g[0] -= self.p * self.l_q / self.l_d * x[self.local_idx['i_sq']]
-        g[1] += self.p * self.l_d / self.l_q * x[self.local_idx['i_sd']]
-
+        # Unpack model constants
+        model_constants= self.physical_system.electrical_motor._model_constants   
+     
         # Pass everything needed to _simulate_sequence
         _, best_sequence = self._simulate_sequence(
-            x, A, g, omega, ref_i_d, ref_i_q, depth=0
+            model_constants,x,omega,omega_Isd,omega_Isq, ref_i_d, ref_i_q, depth=0
         )
         best_idx = best_sequence[0] if best_sequence else 0
         return best_idx
